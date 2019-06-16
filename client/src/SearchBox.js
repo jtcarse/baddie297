ffa69cdd9ab9d0e418d8171df9106cb94b5b28f1
John Carse <jtcarse@gmail.com>
2019-06-16 11:46:49 -0500

    added querying to SearchBox

--- client/src/SearchBox.js
+++ client/src/SearchBox.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Container, Grid } from 'semantic-ui-react';
+import { Button, Container, Grid } from 'semantic-ui-react';
 import AwakeningBox from './AwakeningBox.js';
+import axios from 'axios';
 //import './App.css';
 
 class SearchBox extends Component {
@@ -22,6 +23,17 @@ class SearchBox extends Component {
 		});
 	}
 
+	search() {
+		let url = 'http://localhost:8080/api/monsters?';
+
+		if (this.state.awakenings.length) {
+			url += 'awakenings=' + this.state.awakenings.join();
+		}
+
+		axios.get(url)
+			.then(result => console.log(result.data));
+	}
+
 	render() {
 		return (
 			<div className="SearchBox">
@@ -32,6 +44,7 @@ class SearchBox extends Component {
 							addAwakening={this.addAwakening.bind(this)}
 							removeAwakening={this.removeAwakening.bind(this)}
 						/>
+						<Button onClick={this.search.bind(this)}>Search</Button>
 					</Grid.Column>
 					<Grid.Column mobile={16} tablet={7} computer={7}>
 						<Container>Search Results</Container>
