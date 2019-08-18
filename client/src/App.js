121d6ee06a9d50b589ff0833656ff78db5375b2e
John Carse <jtcarse@gmail.com>
2019-08-17 23:02:02 -0500

    logo and favicon updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,15 @@
 import React, { Component } from 'react';
-//import {} from 'semantic-ui-react';
-//import logo from './logo.svg';
+import { Image } from 'semantic-ui-react';
+import logo from './logo.png';
 import './App.css';
 import SearchBox from './SearchBox.js';
-import axios from 'axios';
 
 class App extends Component {
-	state = { message: null };
-
-	componentDidMount() {
-		axios.get('http://localhost:8080')
-			.then(result => this.setState({ message: result.data.message }));
-	}
-
 	render() {
-		const { message } = this.state;
 		return (
 			<div className="App">
 				<header className="App-header">
-					{/* <img src={logo} className="App-logo" alt="logo" /> */}
-					{ message ? <p>{message}</p> : <p>Loading...</p> }
-					{/*<a
-						className="App-link"
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						<Button>Learn React</Button>
-					</a>*/}
+					<Image src={logo} />
 				</header>
 				<div className="App-body">
 					<SearchBox />
