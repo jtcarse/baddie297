7020aad7dd170339521bf798af442c267de73ecf
John Carse <jtcarse@gmail.com>
2019-11-03 13:42:05 -0600

    added pylintrc, linted server/server.py

--- server/server.py
+++ server/server.py
@@ -1,7 +1,7 @@
+"""Flask API"""
+from pymongo import MongoClient
 from flask import Flask, jsonify, request
 from flask_cors import cross_origin
-from pymongo import MongoClient
-from datetime import datetime
 
 app = Flask(__name__)
 client = MongoClient('mongo', 27017)
@@ -10,63 +10,65 @@ db = client.db
 @app.route('/', methods=['GET'])
 @cross_origin()
 def root():
-	return jsonify(
-		{ 'message': 'There are {} monsters in the database.'.format(db.monsters.count()) }
-	)
+    """Default route of API with summary information."""
+    return jsonify(
+        {'message': 'There are {} monsters in the database.'.format(db.monsters.count())}
+    )
 
 @app.route('/api/monsters', methods=['GET'])
 @cross_origin()
 def monsters():
-	# split up lists within queries
-	args = { key: value.split(',') for key, value in request.args.items() }
+    """Route to search monsters"""
+    # split up lists within queries
+    args = {key: value.split(',') for key, value in request.args.items()}
 
-	# valid parameters: _id, name, skill, awakenings, super_awakenings
-	query_args = {}
+    # valid parameters: _id, name, skill, awakenings, super_awakenings
+    query_args = {}
 
-	# insert types into query
-	if 'types' in args.keys():
-		# check whether to 'and'/'or' types
-		if 'type_logic' in args.keys() and args['type_logic'][0] == 'or':
-			query_args['types'] = { '$in': [ t for t in args['types'] ] }
-		#default to 'and'
-		else:
-			query_args['types'] = { '$all': [ t for t in args['types'] ] }
+    # insert types into query
+    if 'types' in args.keys():
+        # check whether to 'and'/'or' types
+        if 'type_logic' in args.keys() and args['type_logic'][0] == 'or':
+            query_args['types'] = {'$in': [t for t in args['types']]}
+        #default to 'and'
+        else:
+            query_args['types'] = {'$all': [t for t in args['types']]}
 
-	# insert elements into query
-	if 'elements' in args.keys():
-		# check whether to 'and'/'or' elements
-		if 'element_logic' in args.keys() and args['element_logic'][0] == 'or':
-			query_args['elements'] = { '$in': [ e for e in args['elements'] ] }
-		else:
-			query_args['elements'] = { '$all': [ e for e in args['elements'] ] }
+    # insert elements into query
+    if 'elements' in args.keys():
+        # check whether to 'and'/'or' elements
+        if 'element_logic' in args.keys() and args['element_logic'][0] == 'or':
+            query_args['elements'] = {'$in': [e for e in args['elements']]}
+        else:
+            query_args['elements'] = {'$all': [e for e in args['elements']]}
 
-	# insert awakenings into query
-	if 'awakenings' in args.keys():
-		a_dict = {}
-		for a in args['awakenings']:
-			a_dict[a] = a_dict.get(a, 0) + 1
-		for key, value in a_dict.items():
-			query_args['awakenings.{}'.format(key)] = { '$gte': value }
+    # insert awakenings into query
+    if 'awakenings' in args.keys():
+        a_dict = {}
+        for a in args['awakenings']:
+            a_dict[a] = a_dict.get(a, 0) + 1
+        for key, value in a_dict.items():
+            query_args['awakenings.{}'.format(key)] = {'$gte': value}
 
-	# insert super awakenings into query
-	if 'super_awakenings' in args.keys():
-		query_args['$or'] = [ { 'super_awakenings': int(s) } for s in args['super_awakenings'] ]
+    # insert super awakenings into query
+    if 'super_awakenings' in args.keys():
+        query_args['$or'] = [{'super_awakenings': int(s)} for s in args['super_awakenings']]
 
-	if query_args:
-		query = { '$and': [ { key: value for key, value in query_args.items() } ] }
-	else:
-		query = {}
+    if query_args:
+        query = {'$and': [{key: value for key, value in query_args.items()}]}
+    else:
+        query = {}
 
-	result = db.monsters.find(query, limit=20)
-	monsters = [ monster for monster in result ]
+    result = db.monsters.find(query, limit=20)
 
-	return jsonify(monsters)
+    return jsonify([monster for monster in result])
 
 @app.route('/api/monsters/<int:monster_id>', methods=['GET'])
 @cross_origin()
 def monster(monster_id):
-	return jsonify(db.monsters.find_one({ '_id': monster_id }))
+    """Route for detailed information about a specfic monsters."""
+    return jsonify(db.monsters.find_one({'_id': monster_id}))
 
 if __name__ == '__main__':
-	# only used locally
-	app.run(host='0.0.0.0', port=8080, debug=True)
+    # only used locally
+    app.run(host='0.0.0.0', port=8080, debug=True)
