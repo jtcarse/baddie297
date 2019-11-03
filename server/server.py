7fbb2c2d36bb61866565e37e0db22e4e56b0d702
John Carse <jtcarse@gmail.com>
2019-11-03 14:59:04 -0600

    added backend pagination

--- server/server.py
+++ server/server.py
@@ -1,4 +1,6 @@
 """Flask API"""
+from math import ceil
+
 from pymongo import MongoClient
 from flask import Flask, jsonify, request
 from flask_cors import cross_origin
@@ -19,31 +21,40 @@ def root():
 @cross_origin()
 def monsters():
     """Route to search monsters"""
+    # how many monsters to return per page
+    PAGE_SIZE = 25
+
     # split up lists within queries
     args = {key: value.split(',') for key, value in request.args.items()}
 
-    # valid parameters: _id, name, skill, awakenings, super_awakenings
+    # valid parameters: name, skill, awakenings, super_awakenings, page
     query_args = {}
 
+    # get page number; default to 0
+    if 'page' in args:
+        page = int(args['page'][0])
+    else:
+        page = 0
+
     # insert types into query
-    if 'types' in args.keys():
+    if 'types' in args:
         # check whether to 'and'/'or' types
-        if 'type_logic' in args.keys() and args['type_logic'][0] == 'or':
+        if 'type_logic' in args and args['type_logic'][0] == 'or':
             query_args['types'] = {'$in': [t for t in args['types']]}
         #default to 'and'
         else:
             query_args['types'] = {'$all': [t for t in args['types']]}
 
     # insert elements into query
-    if 'elements' in args.keys():
+    if 'elements' in args:
         # check whether to 'and'/'or' elements
-        if 'element_logic' in args.keys() and args['element_logic'][0] == 'or':
+        if 'element_logic' in args and args['element_logic'][0] == 'or':
             query_args['elements'] = {'$in': [e for e in args['elements']]}
         else:
             query_args['elements'] = {'$all': [e for e in args['elements']]}
 
     # insert awakenings into query
-    if 'awakenings' in args.keys():
+    if 'awakenings' in args:
         a_dict = {}
         for a in args['awakenings']:
             a_dict[a] = a_dict.get(a, 0) + 1
@@ -51,7 +62,7 @@ def monsters():
             query_args['awakenings.{}'.format(key)] = {'$gte': value}
 
     # insert super awakenings into query
-    if 'super_awakenings' in args.keys():
+    if 'super_awakenings' in args:
         query_args['$or'] = [{'super_awakenings': int(s)} for s in args['super_awakenings']]
 
     if query_args:
@@ -59,9 +70,13 @@ def monsters():
     else:
         query = {}
 
-    result = db.monsters.find(query, limit=20)
+    results = [monster for monster in db.monsters.find(query, skip=page*PAGE_SIZE, limit=PAGE_SIZE)]
 
-    return jsonify([monster for monster in result])
+    return jsonify({
+        'current_page': page,
+        'total_pages': ceil(db.monsters.count(query)/25),
+        'data': results
+    })
 
 @app.route('/api/monsters/<int:monster_id>', methods=['GET'])
 @cross_origin()
