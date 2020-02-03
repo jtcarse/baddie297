6616876dbf666a92eb7c7609ebd0e4978fd58987
John Carse <jtcarse@gmail.com>
2020-02-03 02:36:28 +0000

    mobile working except result type/awakening icons are too small

--- client/src/Result.js
+++ client/src/Result.js
@@ -64,7 +64,7 @@ class Result extends Component {
                 <div className="Result">
                     <Grid>
                         <Grid.Row stretched>
-                            <Grid.Column className="IconColumn" width={3}>
+                            <Grid.Column className="IconColumn" width={4}>
                                 <Segment basic className="ResultSegment">
                                     {
                                         this.state.iconUrl ?
@@ -72,25 +72,27 @@ class Result extends Component {
                                             className="MonsterIcon"
                                             src={this.state.iconUrl}
                                             target="_blank"
-                                            href={"http://www.puzzledragonx.com/en/monster.asp?n=" + this.props.id } /> :
-                                        <Loader active />
+                                            href={"http://www.puzzledragonx.com/en/monster.asp?n=" + this.props.id}/> :
+                                        <Loader active/>
                                     }
                                 </Segment>
-                            </Grid.Column>
-                            <Grid.Column width={13}>
                                 <Segment basic className="ResultSegment">
-                                    <List horizontal>
+                                    <List horizontal className="TypeList">
                                     {
                                         this.state.types ?
                                         this.state.types.map(t =>
                                             <List.Item className="ResultItem">
-                                                <Image className="TypeIcon" src={typeIcons[t]} />
+                                                <Image className="TypeIcon" src={typeIcons[t]}/>
                                             </List.Item>
                                         ) :
-                                        <List.Item className="ResultItem"><Image src={placeholder} /></List.Item>
+                                        <List.Item className="ResultItem"><Image src={placeholder}/></List.Item>
                                     }
-                                    <List.Item className="ResultItem"><div className="ResultName">{this.props.name}</div></List.Item>
                                     </List>
+                                </Segment>
+                            </Grid.Column>
+                            <Grid.Column width={12}>
+                                <Segment basic className="ResultSegment">
+                                    <div verticalAlign="middle" className="ResultName">{this.props.name}</div>
                                 </Segment>
                                 <Segment basic className="ResultSegment">
                                     <List horizontal>
@@ -98,10 +100,10 @@ class Result extends Component {
                                         this.state.awakenings ?
                                         this.state.awakenings.map(id =>
                                             <List.Item className="ResultItem">
-                                                <Image className="AwakeningIcon" src={awakeningIcons[id]} />
+                                                <Image className="AwakeningIcon" src={awakeningIcons[id]}/>
                                             </List.Item>
                                         ) :
-                                        <List.Item className="ResultItem"><Image src={placeholder} /></List.Item>
+                                        <List.Item className="ResultItem"><Image src={placeholder}/></List.Item>
                                     }
                                     </List>
                                 </Segment>
@@ -111,10 +113,10 @@ class Result extends Component {
                                         this.state.super_awakenings ?
                                         this.state.super_awakenings.map(id =>
                                             <List.Item className="ResultItem">
-                                                <Image className="AwakeningIcon" src={awakeningIcons[id]} />
+                                                <Image className="AwakeningIcon" src={awakeningIcons[id]}/>
                                             </List.Item>
                                         ) :
-                                        <List.Item className="ResultItem"><Image src={placeholder} /></List.Item>
+                                        <List.Item className="ResultItem"><Image src={placeholder}/></List.Item>
                                     }
                                     </List>
                                 </Segment>
